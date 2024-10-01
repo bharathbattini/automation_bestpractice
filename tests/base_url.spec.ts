c7dec9311388b4fed3ca6c64e7b9741303f30f47
Bharath Battini <bharath.bathini@atriina.com>
2024-10-01 11:34:57 +0530

    Bharath: first commit

---
+++ tests/base_url.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Config the Base URL using .env', () => {
+
+    test('Open the 1 Finance Base URL', async ({ page }) => {
+        console.log(process.env.BASEURL);
+        await page.goto(String(process.env.BASEURL));
+
+    });
+
+});
---
+++ tests/best_practices/locators.spec.ts
@@ -0,0 +1,52 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach('', async ({ page }) => {
+
+    await page.goto(String(process.env.BASEURL));
+
+});
+
+const HEADERNAME = {
+
+    calculators: 'Calculators',
+    scoringAndRanking: 'Scoring & Ranking',
+    moneySign: 'MoneySign®',
+    blogs: 'Blogs',
+    community: 'Community',
+    about: 'About',
+
+} as const
+
+
+const SUBHEADERNAME = {
+
+    ourStory: 'Our Story',
+    ourQFA: 'Our QFA',
+    events: 'Events',
+    careers: 'Careers',
+    community: 'Community',
+    about: 'About',
+
+} as const
+
+
+test.describe('Example of using Best Locators on each element', () => {
+
+    test('Check the Title of Page', async ({ page }) => {
+
+        let titleName = 'Personal Financial Management & Planning - Best Personal Finance Advisory in India | 1 Finance';
+
+        await expect(page).toHaveTitle(titleName);
+
+    });
+
+    test.only('To check the Headers are clickable', async ({ page }) => {
+
+        await page.getByText(HEADERNAME.calculators).click();
+        await expect(page).toHaveURL('calculator');
+
+    });
+
+
+
+});
