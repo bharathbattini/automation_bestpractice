da424b4f10fe53005495eea48cfa203a255a65c6
Bharath Battini <bharath.bathini@atriina.com>
2024-10-03 13:15:30 +0530

    added .env files

--- tests/base_url.spec.ts
+++ tests/base_url.spec.ts
@@ -5,7 +5,7 @@ test.describe('Config the Base URL using .env', () => {
     test('Open the 1 Finance Base URL', async ({ page }) => {
         console.log(process.env.BASEURL);
         await page.goto(String(process.env.BASEURL));
-
+        await expect(page).toHaveURL(String(process.env.BASEURL));
     });
 
 });
